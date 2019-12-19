85289740d1bcb99a8961908794db91f999aa155b
sn49 <34567701+sn49@users.noreply.github.com>
2019-12-19 10:26:34 +0900

    [Modify] image road test

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import cv2
-import numpy
+import numpy as np
 import matplotlib
+
+def showImage():
+    imgfile = 'D:/paintings/cave1_bg3.png'
+    img = cv2.imread(imgfile,cv2.IMREAD_COLOR)
+
+    cv2.imshow('background',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+showImage()
+
+print(1000000000000)
