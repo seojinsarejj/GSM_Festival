091f6ba67831193865f49404636057f02e139ba7
seojinsarejj <s19009@naver.com>
2019-12-19 16:51:57 +0900

    [UPDATE] update eye box color

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ def crop_eye(img, eye_points):
   return eye_img, eye_rect
 
 # main
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while cap.isOpened():
   ret, img_ori = cap.read()
@@ -39,7 +39,7 @@ while cap.isOpened():
   if not ret:
     break
 
-  img_ori = cv2.resize(img_ori, dsize=(0, 0), fx=0.5, fy=0.5)
+  img_ori = cv2.resize(img_ori, dsize=(0, 0), fx=1.2, fy=1.2)
 
   img = img_ori.copy()
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -57,8 +57,6 @@ while cap.isOpened():
     eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
     eye_img_r = cv2.flip(eye_img_r, flipCode=1)
 
-    cv2.imshow('l', eye_img_l)
-    cv2.imshow('r', eye_img_r)
 
     eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
     eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
@@ -67,18 +65,29 @@ while cap.isOpened():
     pred_r = model.predict(eye_input_r)
 
     # visualize
-    state_l = 'O %.1f' if pred_l > 0.1 else '- %.1f'
-    state_r = 'O %.1f' if pred_r > 0.1 else '- %.1f'
+    state_l = 'O' if pred_l > 0.1 else '-'
+    state_r = 'O' if pred_r > 0.1 else '-'
 
     state_l = state_l % pred_l
     state_r = state_r % pred_r
 
-    cv2.rectangle(img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=(255,255,255), thickness=2)
-    cv2.rectangle(img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=(255,255,255), thickness=2)
+    if state_l == 'O':
+      cv2.rectangle(img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=(255,0,0), thickness=2)
+    else :
+      cv2.rectangle(img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=(0,0,255), thickness=2)
 
+
+    if state_r == 'O':
+      cv2.rectangle(img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=(255,0,0), thickness=2)
+    else :
+      cv2.rectangle(img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=(0,0,255), thickness=2)
+          
+  
     cv2.putText(img, state_l, tuple(eye_rect_l[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
     cv2.putText(img, state_r, tuple(eye_rect_r[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
 
   cv2.imshow('result', img)
   if cv2.waitKey(1) == ord('q'):
     break
+
+
